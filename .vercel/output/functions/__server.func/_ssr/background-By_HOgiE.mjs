6fcdba0ff09bbab43870fa178ad3f7cfc51c426b
Praveen Phatak <praveenphatak@gmail.com>
2026-03-26 14:16:11 -0300

    highlight build

---
+++ .vercel/output/functions/__server.func/_ssr/background-By_HOgiE.mjs
@@ -0,0 +1,30 @@
+import { j as jsxRuntimeExports } from "../_chunks/_libs/react.mjs";
+import { c as cn } from "./router-D7uwePGH.mjs";
+function Background({ className, children, type = "dot" }) {
+  return /* @__PURE__ */ jsxRuntimeExports.jsxs(
+    "div",
+    {
+      className: cn(
+        "relative flex h-full w-full items-center justify-center bg-background",
+        className
+      ),
+      children: [
+        /* @__PURE__ */ jsxRuntimeExports.jsx(
+          "div",
+          {
+            className: cn(
+              "absolute inset-0",
+              "bg-size-[40px_40px]",
+              type === "dot" ? "bg-[radial-gradient(#404040_1px,transparent_1px)]" : "bg-[linear-gradient(to_right,#262626_1px,transparent_1px),linear-gradient(to_bottom,#262626_1px,transparent_1px)]"
+            )
+          }
+        ),
+        /* @__PURE__ */ jsxRuntimeExports.jsx("div", { className: "pointer-events-none absolute inset-0 flex items-center justify-center bg-background mask-[radial-gradient(ellipse_at_center,transparent_20%,black)]" }),
+        /* @__PURE__ */ jsxRuntimeExports.jsx("div", { className: "relative z-20 w-full", children })
+      ]
+    }
+  );
+}
+export {
+  Background as B
+};
---
+++ .vercel/output/functions/__server.func/_ssr/hooks-BgvLkfJ2.mjs
@@ -0,0 +1,42 @@
+import { b as useQueryClient, c as useMutation } from "../_chunks/_libs/@tanstack/react-query.mjs";
+import { u as useRouter } from "../_chunks/_libs/@tanstack/react-router.mjs";
+import { z as zt } from "../_libs/react-hot-toast.mjs";
+import { u as useModal } from "./modal-rPNNRycr.mjs";
+import { s as userKeys } from "./router-D7uwePGH.mjs";
+import { u as upsertProfile, f as activateProfile } from "./services-DjdOH3KL.mjs";
+function useUpsertProfile() {
+  const queryClient = useQueryClient();
+  const router = useRouter();
+  const { closeModal, modalId } = useModal();
+  return useMutation({
+    mutationFn: upsertProfile,
+    onSuccess: (_, { data: { update } }) => {
+      queryClient.invalidateQueries({ queryKey: userKeys.all });
+      closeModal(modalId);
+      router.invalidate();
+      zt.success(`Profile ${update ? "updated" : "created"} successfully`);
+    },
+    onError: (error) => {
+      zt.error(error.message ?? "Could not process request");
+    }
+  });
+}
+function useActivateProfile() {
+  const queryClient = useQueryClient();
+  const router = useRouter();
+  return useMutation({
+    mutationFn: activateProfile,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: userKeys.all });
+      router.invalidate();
+      zt.success(`Profile activated successfully`);
+    },
+    onError: (error) => {
+      zt.error(error.message ?? "Could not process request");
+    }
+  });
+}
+export {
+  useUpsertProfile as a,
+  useActivateProfile as u
+};
